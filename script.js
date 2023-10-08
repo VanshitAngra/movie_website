4c67e90c0762acf35b667cccdcda24d5a71b327b
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-10-09 02:55:00 +0530

    modification of html and javascript

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
-// http://www.omdbapi.com/?s=avengers&age=1&apikey=8652c1ca
+// http://www.omdbapi.com/?s=avengers&page=1&apikey=8652c1ca
 // http://www.omdbapi.com/?i=tt3896198&apikey=8652c1ca
-const movieSearchBox=document.getElementById('movie-search-box');
-const seachList=document.getElementById('search-list');
-const resultGrid=document.getElementById('result-grid');
+const movieSearchBox=document.getElementById('search-movie');
+const searchList=document.getElementsByClassName('search-list')[0];
+const resultGrid=document.getElementById('resultGrid');
+
+async function loadMovies(searchTerm){
+    const URL=`http://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=8652c1ca`;
+    const res=await fetch(`${URL}`);
+    const data=await res.json();
+    // console.log(data);
+    if(data.Response == "True")
+    displayMovieList(data.Search);
+}
+function findMovies(){
+    let searchTerm=(movieSearchBox.value).trim();
+    if(searchTerm.length > 0)
+    {
+        searchList.classList.remove('hide-search-list');
+        loadMovies(searchTerm);
+    }
+    else{
+        searchList.classList.add('hide-search-list');
+    }
+
+}
+
+function displayMovieList(movies){
+    searchList.innerHTML="";
+    for(let i=0;i<movies.length;i++){
+        let movieListItem=document.createElement('div');
+        movieListItem.dataset.id=movies[i].imdbID;
+        movieListItem.classList.add('search-item');
+        if(movies[i].Poster !="N/A")
+            moviePoster=movies[i].Poster;
+        else
+        moviePoster="./imgs/image.png";
+
+        movieListItem.innerHTML=`
+        <div class="thumbnail">
+        <img src="${moviePoster}" alt="">
+    </div>
+    <div class="search-info">
+        <h3>${movies[i].Title}</h3>
+        <p>${movies[i].Year}</p>
+    </div>`;
+    searchList.appendChild(movieListItem);
+
+    }
+}
